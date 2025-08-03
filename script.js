7575642f834c59af2d79b14839ae47133a6caaa7
alijalilzadeh <ali.jalilzadeh2007@gmail.com>
2025-08-03 15:05:33 +0400

    feat : added currency conversion for Turkish Lira (TRY)

--- script.js
+++ script.js
@@ -4,20 +4,39 @@ const selection2 = document.querySelector("#currency-option2");
 const result = document.querySelector("#result");
 
 
+
 const calculateButton = document.querySelector("#calculatebtn").addEventListener("click", function(){
   // console.log("Calculate button clicked");
   const value1 = parseFloat(input.value);
   let convertedCurrency;
-  if(selection1.value === "Dollar" && selection2.value === "Manat"){
+  if(selection1.value === "USD" && selection2.value === "AZN"){
       convertedCurrency = value1*1.7;
       result.innerText = `${value1} ${selection1.value} = ${convertedCurrency} ${selection2.value}`;
   }
-    if(selection1.value === "Manat" && selection2.value === "Dollar"){
+    if(selection1.value === "AZN" && selection2.value === "USD"){
       convertedCurrency = value1/1.7;
       result.innerText = `${value1} ${selection1.value} = ${convertedCurrency.toFixed(2)} ${selection2.value}`;
   }
- 
- 
+  if(selection1.value === "AZN" && selection2.value === "TRY"){
+    convertedCurrency = value1*23.92;
+    result.innerText = `${value1} ${selection1.value} = ${convertedCurrency.toFixed(2)} ${selection2.value} `;
+  }
+  if(selection1.value === "TRY" && selection2.value === "AZN"){
+    convertedCurrency = value1/23.92;
+    result.innerText = `${value1} ${selection1.value} = ${convertedCurrency.toFixed(2)} ${selection2.value} `;
+  }
+  
+    if(selection1.value === "USD" && selection2.value === "TRY"){
+    convertedCurrency = value1*40.65;
+    result.innerText = `${value1} ${selection1.value} = ${convertedCurrency.toFixed(2)} ${selection2.value} `;
+  }
+    if(selection1.value === "TRY" && selection2.value === "USD"){
+    convertedCurrency = value1/40.65;
+    result.innerText = `${value1} ${selection1.value} = ${convertedCurrency.toFixed(2)} ${selection2.value} `;
+  }
+  
+  
 });
+
 
 
